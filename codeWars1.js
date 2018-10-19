9f1d9bb9060515707859045ebb4611bb6dc13f18
Kyle <43317436+KyleR44@users.noreply.github.com>
2018-10-19 19:55:58 -0400

    Add files via upload

---
+++ codeWars1.js
@@ -0,0 +1,7 @@
+// In javascript, your code is running in a function, let us step by step complete your first JS function.
+
+// this function displays a msg that says helloWorld
+function helloWorld(){
+var str = "Hello World";
+console.log(str)
+}
